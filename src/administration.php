f66f6e7d6a21eebee9886ee13558b398155062fa
yuniegg <maryline.saladin@gmail.com>
2018-03-28 15:40:30 +0200

    remove unecessary js folder and file, creation of denied access page, and handling of rights to see profile and administration pages

--- src/administration.php
+++ src/administration.php
@@ -1,10 +1,19 @@
 <?php
 include_once "../includes/layout_header.php";
-?>
+
+if($_SESSION['logged'] && $_SESSION['role'] == ADMINISTRATOR) {
+    ?>
 
     <!-- Contenu -->
     <div>
         <h1> Administration </h1>
     </div>
 
-<?php include_once "../includes/layout_footer.php"; ?>
+    <?php
+}
+else {
+    echo $twig->render("page_access_denied.html");
+}
+
+include_once "../includes/layout_footer.php";
+?>
--- src/profile.php
+++ src/profile.php
@@ -1,10 +1,19 @@
 <?php
-include_once "../includes/layout_header.php";
+    include_once "../includes/layout_header.php";
+
+    if($_SESSION['logged']) {
+        ?>
+
+        <!-- Contenu -->
+        <div>
+            <h1> Profile </h1>
+        </div>
+
+        <?php
+    }
+    else {
+        echo $twig->render("page_access_denied.html");
+    }
+
+    include_once "../includes/layout_footer.php";
 ?>
-
-    <!-- Contenu -->
-    <div>
-        <h1> Profile </h1>
-    </div>
-
-<?php include_once "../includes/layout_footer.php"; ?>
